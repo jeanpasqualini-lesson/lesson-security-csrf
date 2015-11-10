d463a65d750d3fda1ac2fd95694d646b3add3c6c
jean pasqualini <jean.pasqualini.abbeal@gmail.com>
2015-11-10 10:01:42 +0100

    first commit

---
+++ src/Test/MainTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: prestataire
+ * Date: 09/11/15
+ * Time: 17:28
+ */
+
+namespace Test;
+
+use \Interfaces\TestInterface;
+
+class MainTest implements TestInterface
+{
+    public function runTest()
+    {
+
+    }
+}
